e8a8e0dd4d8128bc3c04f125531eed8b076f3d02
RazanHamza <Kabbaskhaled116@gmail.com>
2025-10-03 11:08:52 +0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,5 +17,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/cart/:path*", "/wishList/:path*"], 
+  matcher: ["/cart/:path*", "/wishList/:path*"],
 };
